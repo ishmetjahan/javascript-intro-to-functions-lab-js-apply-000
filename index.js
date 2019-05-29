cb26695c67b1121a0cb104839e9d35d7179e2279
Ishmet <ishmetjahan@yahoo.com>
2019-05-29 20:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ function logShout(str2){
 function logWhisper(str3){
   console.log(str3.toLowerCase())
 }
-function sayHiToGrandma(str4){
-  if (str4===str4.toLowerCase()){
+function sayHiToGrandma(str){
+  if (str===str.toLowerCase()){
     return "I can't hear you!"
   }
-}
-function sayHiToGrandma(str5){
+  if (str===)
   
 }
